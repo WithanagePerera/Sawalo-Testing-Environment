451ad224619a6d9b9049af7d76c2d8a16de9025e
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-09-16 12:11:19 -0400

    Created subject validation tests.

---
+++ index.js
@@ -0,0 +1,43 @@
+const subjectResult = document.querySelector('#subject-result');
+const url = window.location.href.toLowerCase();
+const html = document.documentElement.innerHTML;
+
+let title = html.substring(html.indexOf("<title>"), html.indexOf("</title>")).toLowerCase();
+let header = html.substring(html.indexOf("<h1"), html.indexOf("</h1>")).toLowerCase();
+
+var scores = new Array(2);
+scores[0] = new Array("english", "math", "chemistry", "biology", "physics", "history", "spanish");
+scores[1] = new Array(scores[0].length);
+
+for (var i = 0; i < scores[0].length; i++)
+    scores[1][i] = 0;
+
+for (var i = 0; i < scores[0].length; i++)
+{
+    if (url.includes(scores[0][i]))
+        scores[1][i] += 1;
+
+    if (title.includes(scores[0][i]))
+        scores[1][i] += 1;
+
+    if (header.includes(scores[0][i]))
+        scores[1][i] += 1;
+}
+
+let x = Number.MIN_VALUE;
+let subject;
+
+for (var i = 0; i < scores[1].length; i++)
+{
+    if (scores[1][i] > x)
+    {
+        x = scores[1][i];
+        subject = scores[0][i];
+    }    
+}
+
+
+subjectResult.innerHTML = 
+`
+    <h2>Tests validate ${subject} as the subject of the page.</h2>
+`;
