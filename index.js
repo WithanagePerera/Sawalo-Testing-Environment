61df1017d712f5287acbd00bf3ca7bd9808140b1
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-09-16 12:14:27 -0400

    Some comments.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ scores[1] = new Array(scores[0].length);
 for (var i = 0; i < scores[0].length; i++)
     scores[1][i] = 0;
 
+// Calculates scores based on URL, HTML title element, and HTML header element
 for (var i = 0; i < scores[0].length; i++)
 {
     if (url.includes(scores[0][i]))
@@ -24,6 +25,7 @@ for (var i = 0; i < scores[0].length; i++)
         scores[1][i] += 1;
 }
 
+// Finds largest score and associated subject
 let x = Number.MIN_VALUE;
 let subject;
 
@@ -36,7 +38,7 @@ for (var i = 0; i < scores[1].length; i++)
     }    
 }
 
-
+// Injects HTML stating subject detected.
 subjectResult.innerHTML = 
 `
     <h2>Tests validate ${subject} as the subject of the page.</h2>
